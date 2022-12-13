ba0a29767ba4734dc52f3392095c14dd5d28065a
Sudesh Choudhary <sudesh.choudhary@kelltontech.org>
2022-12-13 17:57:50 +0530

    header completed

--- src/components/Project/Header.tsx
+++ src/components/Project/Header.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import logo from "../../assets/images/Vector.svg";
+import logo from "../../assets/images/logo.svg";
 import Avatar from "../../assets/images/Avatar.svg";
-import bellIcon from "../../assets/images/Bellicon.svg";
-import searchIcon from "../../assets/images/searchIcon.svg";
+import bellIcon from "../../assets/images/bell.svg";
+import searchIcon from "../../assets/images/Search.svg";
 import { PlusCircleIcon } from "@heroicons/react/20/solid";
 import {
   BanknotesIcon,
@@ -12,54 +12,53 @@ import {
 
 const Header = () => {
   return (
-    <div className="border border-lightGrayColor bg-white shadow-lg px-10 py-4  flex  justify-between items-center">
+    <div className=" h-16 bg-white shadow-sm	shadow-black-500/60 px-10  flex  justify-between items-center">
       <div className="flex space-x-2 items-center">
-        <img src={logo} alt="logo" className="inline-block" />
-        <h1 className="text-textBlack font-inter font-bold text-2xl">
-          KELLTON
-        </h1>
-        <ul className="flex  flex-1 ml-10 text-sm font-inter text-lightGrayColor  space-x-6 ">
-          <li className="ml-10 cursor-pointer hover:text-blackColor hover:border-b-2 hover:border-blueColor">
-            Dashboard
+        <a href=""><img src={logo} alt="logo"/></a>
+        <ul className="flex h-16 font-medium	 flex-1 ml-10 text-sm font-inter text-lightGrayColor  space-x-6 ">
+          <li className="ml-10 flex items-center cursor-pointer   hover:text-blackColor border-b-2 border-transparent hover:border-blueColor">
+           <a href="#">Dashboard</a> 
           </li>
-          <li className="cursor-pointer  hover:border-b-2 hover:border-blueColor hover:text-blackColor">
+          <li className="ml-10 flex items-center cursor-pointer   hover:text-blackColor border-b-2 border-transparent hover:border-blueColor">
             Utilization
           </li>
 
-          <li className="cursor-pointer hover:text-blackColor  hover:border-b-2 hover:border-blueColor">
+          <li className="ml-10 flex items-center cursor-pointer   hover:text-blackColor border-b-2 border-transparent hover:border-blueColor">
             People
           </li>
-          <li className="cursor-pointer hover:text-blackColor hover:border-b-2 hover:border-blueColor">
+          <li className="ml-10 flex items-center cursor-pointer   hover:text-blackColor border-b-2 border-transparent hover:border-blueColor">
             Projects
           </li>
-          <li className="cursor-pointer hover:text-blackColor hover:border-b-2 hover:border-blueColor">
+          <li className="ml-10 flex items-center cursor-pointer   hover:text-blackColor border-b-2 border-transparent hover:border-blueColor">
             Revenue
           </li>
-          <li className="cursor-pointer hover:text-blackColor hover:border-b-2 hover:border-blueColor">
+          <li className="ml-10 flex items-center cursor-pointer   hover:text-blackColor border-b-2 border-transparent hover:border-blueColor">
             Reports
           </li>
         </ul>
-        {/* <div  className="w-10 h-1  bg-yellow-500" /> */}
       </div>
-      <div className="flex justify-end  items-center flex-1 space-x-5">
-        <div className="flex relative">
+      <div className="flex justify-end  items-center flex-1">
+        <div className="flex relative mx-4">
           <img
             src={searchIcon}
             alt=""
-            className="absolute top-3 left-2 w-5 h-5"
+            className="absolute top-0 bottom-0 left-4 w-5 h-5 m-auto"
           />
           <input
             placeholder=""
-            className="border  outline-none text-center placeholder:mr-3 border-gray-300 w-64 p-2 rounded-md"
+            className="border  outline-none text-left pl-12 placeholder:mr-3 border-gray-300 w-64 p-2 rounded-md"
           />
         </div>
-        <div className=" relative">
-          <img src={bellIcon} alt="bellIcon" className="w-8 min-w-7 h-8" />
+      <div className="relative w-7 h-7 mr-4 flex items-center">
+          <img src={bellIcon} alt="bellIcon" className="w-6 h-6" />
           <div className="absolute top-0 right-0 bg-red-500 rounded-full w-4 h-4 text-center">
-            <p className="text-xs">5</p>
+            <p className="text-xs font-semibold	text-white">5</p>
           </div>
         </div>
-        <img src={Avatar} alt="avatar" className="w-10 h-10 object-contain" />
+        <div className="w-8 h-8 overflow-hidden rounded-full	">
+        <img src={Avatar} alt="avatar"/>
+        </div>
+       
       </div>
     </div>
   );
